847c150f034bdf8cb42bbf68ff6bdc83ca4a0543
Ned Zimmerman <ned@bight.ca>
2017-11-29 17:29:16 -0400

    Demonstrate multisite support.

---
+++ resources/footer.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Add support for WordPress Multisite's wp-signup.php
+ * Do not edit anything in this file unless you know what you're doing
+ */
+
+echo \App\template('partials/footer');
---
+++ resources/header.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Add support for WordPress Multisite's wp-signup.php
+ * Do not edit anything in this file unless you know what you're doing
+ */
+
+echo \App\template('partials/head');
+echo \App\template('partials/header');
